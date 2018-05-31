98517a7f37d8129f8819060ff13bd5f69c664801
Eric Wong <erichlhuang@tencent.com>
2018-05-31 19:35:48 +0800

    更新fetch API

--- src/api/util.js
+++ src/api/util.js
@@ -5,4 +5,13 @@ export function fetchAPI(path, options, cb) {
         }).catch((err) => {
             console.log(err);
         })
+
+    // fetch("http://111.231.245.136:80/" + path, {
+    //     method: 'POST',
+    //     headers: {
+    //         "content-type": "application/x-www-form-urlencoded"
+    //     },
+    //     body: "name=eric",
+    //     mode: 'cors'
+    // })
 }
